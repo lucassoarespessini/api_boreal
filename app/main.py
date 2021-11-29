88a9462893d44796423b664f4d9e04a044653206
Lucas Soares Pessini <lucassoarespessini@gmail.com>
2021-11-29 19:38:15 -0300

    Now I put token in all

--- app/main.py
+++ app/main.py
@@ -113,30 +113,8 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
 
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
 
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 async def get_current_active_item(current_item: Item ):
     credentials_exception = HTTPException(
@@ -172,22 +150,28 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-
-@app.get("/users/me/", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return current_user
-
-
-@app.get("/users/me/items/")
-async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
 @app.post("/items/")
 async def create_item(item: Item = Depends(get_current_active_item)):
     return item
 
-@app.get("/cervejas/")
-def cervejas():
-     r = requests.get('https://api.openbrewerydb.org/breweries/')
-     r.json()
-     return r.json()
+@app.get("/cervejas/{token}")
+def read_item(token: str):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    r = requests.get('https://api.openbrewerydb.org/breweries/')
+    r.json()
+    return r.json()
